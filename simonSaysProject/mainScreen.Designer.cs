1526f5f62ce03085ee8f26999c098f5d64f29ea5
Npatton444 <noahpatt222@ed.amdsb.ca>
2016-02-25 10:12:18 -0500

    Made Main Screen. Started main screen design. Main screen runs on start.

---
+++ simonSaysProject/mainScreen.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace simonSaysProject
+{
+    partial class mainScreen
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(mainScreen));
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.startButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.simonImageBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.simonImageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.Color.White;
+            this.titleLabel.Location = new System.Drawing.Point(30, 32);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(225, 38);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Simon Says";
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(37, 89);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(85, 38);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Font = new System.Drawing.Font("Verdana", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.closeButton.Location = new System.Drawing.Point(37, 155);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(85, 38);
+            this.closeButton.TabIndex = 2;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            // 
+            // simonImageBox
+            // 
+            this.simonImageBox.Image = ((System.Drawing.Image)(resources.GetObject("simonImageBox.Image")));
+            this.simonImageBox.ImageLocation = "";
+            this.simonImageBox.Location = new System.Drawing.Point(3, -71);
+            this.simonImageBox.Name = "simonImageBox";
+            this.simonImageBox.Size = new System.Drawing.Size(581, 548);
+            this.simonImageBox.TabIndex = 3;
+            this.simonImageBox.TabStop = false;
+            // 
+            // mainScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.simonImageBox);
+            this.Name = "mainScreen";
+            this.Size = new System.Drawing.Size(600, 600);
+            ((System.ComponentModel.ISupportInitialize)(this.simonImageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.PictureBox simonImageBox;
+    }
+}
