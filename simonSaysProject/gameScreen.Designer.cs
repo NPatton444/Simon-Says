6597ef492402f5e7adad9a16f143dd1452bd9019
Npatton444 <noahpatt222@ed.amdsb.ca>
2016-02-26 09:46:53 -0500

    Finished game screen Layout.

--- simonSaysProject/gameScreen.Designer.cs
+++ simonSaysProject/gameScreen.Designer.cs
@@ -29,6 +29,10 @@
         private void InitializeComponent()
         {
             this.scoreLabel = new System.Windows.Forms.Label();
+            this.greenButton = new System.Windows.Forms.Button();
+            this.redButton = new System.Windows.Forms.Button();
+            this.blueButton = new System.Windows.Forms.Button();
+            this.yellowButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // scoreLabel
@@ -42,11 +46,53 @@
             this.scoreLabel.TabIndex = 1;
             this.scoreLabel.Text = "Score: ";
             // 
+            // greenButton
+            // 
+            this.greenButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.greenButton.FlatAppearance.BorderSize = 0;
+            this.greenButton.Location = new System.Drawing.Point(7, 142);
+            this.greenButton.Margin = new System.Windows.Forms.Padding(0, 0, 0, 0);
+            this.greenButton.Name = "greenButton";
+            this.greenButton.Size = new System.Drawing.Size(200, 200);
+            this.greenButton.TabIndex = 2;
+            this.greenButton.UseVisualStyleBackColor = false;
+            // 
+            // redButton
+            // 
+            this.redButton.BackColor = System.Drawing.Color.Red;
+            this.redButton.Location = new System.Drawing.Point(210, 142);
+            this.redButton.Name = "redButton";
+            this.redButton.Size = new System.Drawing.Size(200, 200);
+            this.redButton.TabIndex = 3;
+            this.redButton.UseVisualStyleBackColor = false;
+            // 
+            // blueButton
+            // 
+            this.blueButton.BackColor = System.Drawing.Color.Blue;
+            this.blueButton.Location = new System.Drawing.Point(7, 345);
+            this.blueButton.Name = "blueButton";
+            this.blueButton.Size = new System.Drawing.Size(200, 200);
+            this.blueButton.TabIndex = 4;
+            this.blueButton.UseVisualStyleBackColor = false;
+            // 
+            // yellowButton
+            // 
+            this.yellowButton.BackColor = System.Drawing.Color.Yellow;
+            this.yellowButton.Location = new System.Drawing.Point(210, 345);
+            this.yellowButton.Name = "yellowButton";
+            this.yellowButton.Size = new System.Drawing.Size(200, 200);
+            this.yellowButton.TabIndex = 5;
+            this.yellowButton.UseVisualStyleBackColor = false;
+            // 
             // gameScreen
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DimGray;
+            this.Controls.Add(this.yellowButton);
+            this.Controls.Add(this.blueButton);
+            this.Controls.Add(this.redButton);
+            this.Controls.Add(this.greenButton);
             this.Controls.Add(this.scoreLabel);
             this.Name = "gameScreen";
             this.Size = new System.Drawing.Size(600, 600);
@@ -58,5 +104,9 @@
         #endregion
 
         private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Button greenButton;
+        private System.Windows.Forms.Button redButton;
+        private System.Windows.Forms.Button blueButton;
+        private System.Windows.Forms.Button yellowButton;
     }
 }
--- simonSaysProject/gameScreen.cs
+++ simonSaysProject/gameScreen.cs
@@ -15,6 +15,12 @@ namespace simonSaysProject
         public gameScreen()
         {
             InitializeComponent();
+
+            //Button Locations
+            yellowButton.Location = new Point(this.Width / 2, this.Height / 2);
+            blueButton.Location = new Point(this.Width / 2 - blueButton.Width, this.Height / 2);
+            redButton.Location = new Point(this.Width / 2, this.Height / 2 - redButton.Height);
+            greenButton.Location = new Point(this.Width / 2 - greenButton.Width, this.Height / 2 - greenButton.Height);
         }
     }
 }
